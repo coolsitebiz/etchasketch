7bc0a9f4d6a761bc2835e0fd0707a38da3dd6a9d
bryan <32003096+coolsitebiz@users.noreply.github.com>
2022-09-07 23:29:50 -0700

    Use css grid instead of flexbox with wrap

--- scripts.js
+++ scripts.js
@@ -10,21 +10,39 @@ function makeGrid(dimension) {
         dimension = 100;
     }
 
-    let container = document.querySelector('#grid');
-    // let style = window.getComputedStyle(container);  testing dynamic container width
-    // let wh = style.getPropertyValue('width');
-    // console.log(parseInt(wh));
+    let main = document.querySelector('#main');
+    let grid = document.createElement('div');
+    grid.className = 'grid';
+    grid.style.gridTemplateColumns = `repeat(${dimension}, ${640/dimension}px)`;
+    main.appendChild(grid);
+
 
     for (let i = 0; i < dimension * dimension; i++) {
         let cell = document.createElement('div');
         cell.className = 'cell';
-        cell.addEventListener('mouseover', (e) => {
+        cell.addEventListener('mousedown', (e) => {
             changeColor(e)
         })
-        container.appendChild(cell);
+        cell.addEventListener('mouseover', (e) => {
+            e.target.classList.toggle("highlighted");
+        })
+        // cell.addEventListener('mouseout', (e) => {
+        //     e.target.classList.toggle("highlighted");
+        // })
+        grid.appendChild(cell);
     }
 
 
+}
+
+function newGrid(dimension) {
+    destroyGrid();
+    makeGrid(dimension);
+}
+
+function destroyGrid() {
+    let grid = document.querySelector(".grid");
+    grid.parentNode.removeChild(grid);
 }
 
 function changeColor(obj) {
