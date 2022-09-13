dfdce301c884c63ded103e7eb01571a3be29ce03
Bryan Nelson <32003096+coolsitebiz@users.noreply.github.com>
2022-09-13 12:33:18 -0700

    Implement color input for custom colors

--- scripts.js
+++ scripts.js
@@ -1,61 +1,56 @@
-let color = colors.black;
+let color = "black";
 
-function setColor(newColor) {
-    color = newColor;
+function setColor(newColor, e) {
+  color = newColor;
+  e.blur();
 }
 
 function makeGrid(dimension) {
+  if (dimension < 2) {
+    dimension = 2;
+  }
 
-    if (dimension < 2) {
-        dimension = 2;
-    }
+  if (dimension > 100) {
+    dimension = 100;
+  }
 
-    if (dimension > 100) {
-        dimension = 100;
-    }
+  let main = document.querySelector("#main");
+  let grid = document.createElement("div");
+  grid.className = "grid";
+  grid.style.gridTemplateColumns = `repeat(${dimension}, ${640 / dimension}px)`;
+  main.appendChild(grid);
 
-    let main = document.querySelector('#main');
-    let grid = document.createElement('div');
-    grid.className = 'grid';
-    grid.style.gridTemplateColumns = `repeat(${dimension}, ${640/dimension}px)`;
-    main.appendChild(grid);
+  for (let i = 0; i < dimension * dimension; i++) {
+    let cell = document.createElement("div");
+    cell.style.backgroundColor = "white";
+    cell.style.border = "1px dotted gray";
+    cell.style.cursor = "";
+    cell.addEventListener("mousedown", (e) => {
+      changeColor(e, color);
+    });
 
-
-    for (let i = 0; i < dimension * dimension; i++) {
-        let cell = document.createElement('div');
-        cell.style.backgroundColor = "white";
-        cell.style.border = "1px solid gray";
-        cell.addEventListener('mousedown', (e) => {
-            changeColor(e, color);
-        })
-
-        grid.appendChild(cell);
-    }
-
-
+    grid.appendChild(cell);
+  }
 }
 
 function newGrid() {
-    let dimension = prompt("Enter a number between 2 and 100:"); 
-    if (isNaN(dimension)) {
-        dimension = 16;
-    }
-    destroyGrid();
-    makeGrid(dimension);
+  let dimension = prompt("Enter a number between 2 and 100:");
+  if (isNaN(dimension)) {
+    dimension = 16;
+  }
+  destroyGrid();
+  makeGrid(dimension);
 }
 
 function destroyGrid() {
-    let grid = document.querySelector(".grid");
-    grid.parentNode.removeChild(grid);
+  let grid = document.querySelector(".grid");
+  grid.parentNode.removeChild(grid);
 }
 
 function changeColor(obj, color) {
-    if (obj.target.style.backgroundColor !== "white") {
-        obj.target.style.backgroundColor = "white";
-    } else {
-        obj.target.style.backgroundColor = color;
-    }
-    
-    
-    
+  if (obj.target.style.backgroundColor !== "white") {
+    obj.target.style.backgroundColor = "white";
+  } else {
+    obj.target.style.backgroundColor = color;
+  }
 }
